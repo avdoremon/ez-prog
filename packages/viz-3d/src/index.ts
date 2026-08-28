7d5bec6cec0eb4f90d9886203647b5dd9ebba224
avdoremon <nguyenanhmeister@gmail.com>
2026-08-28 16:18:25 +0700

    feat: add the GraphView3D component

--- packages/viz-3d/src/index.ts
+++ packages/viz-3d/src/index.ts
@@ -1,4 +1,10 @@
 export { TreeView3D, type TreeView3DProps } from './TreeView3D.js';
 export { layoutTree3D, type Position3D } from './layout.js';
 export { buildSceneSummary } from './sceneSummary.js';
-export { colorForMarks } from './markColors.js';
+export { colorForMarks, DEFAULT_COLOR, INK_COLOR } from './markColors.js';
+export { GraphView3D, type GraphView3DProps } from './GraphView3D.js';
+export { layoutGraph3D } from './graphLayout.js';
+export { buildGraphSummary } from './graphSummary.js';
+export {
+  describeNeighbors, marksForEdge, marksForNode, neighborsOf, type Neighbor,
+} from './graphAccessibility.js';
